bb481ef7ba86786d45f719e94861e2ffe56c0e28
jojoyal <josepjoyal@gmail.com>
2023-03-04 11:19:56 +0530

    First commit with basic setup of frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv')
+
+const app = express();
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(PORT, ()=>{
+  console.log('Server started at port 3500');
+});
