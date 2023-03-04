ec71ad4a028bcec94afc9649c3cdce3eaf76cbe4
jojoyal <josepjoyal@gmail.com>
2023-03-04 16:04:20 +0530

    Made landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import React from 'react'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage from './screens/LandingPage/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      Hello React
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header/>
 
-export default App;
+    <main>
+    <LandingPage/>
+    </main>
+
+    <Footer/>
+  </>
+)
+
+export default App
